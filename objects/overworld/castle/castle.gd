72d8fe551725c5be1f4db2181ac36c50df9e0ef3
Janez Novak <banana@banana.com>
2026-03-06 16:30:58 +0100

    not walkable done

--- objects/overworld/castle/castle.gd
+++ objects/overworld/castle/castle.gd
@@ -4,3 +4,11 @@ extends Node2D
 @export var player_owner = 1
 
 var base_map: Node = null
+
+func get_pathfinding_blocked_tile_centers() -> Array:
+	return [
+		global_position + Vector2(32, 32),
+		global_position + Vector2(64, 16),
+		global_position + Vector2(64, 48),
+		global_position + Vector2(96, 32)
+	]
--- objects/overworld/town/town.gd
+++ objects/overworld/town/town.gd
@@ -12,6 +12,14 @@ var fields = []
 
 var base_map: Node = null
 
+func get_pathfinding_blocked_tile_centers() -> Array:
+	return [
+		global_position + Vector2(32, 32),
+		global_position + Vector2(64, 16),
+		global_position + Vector2(64, 48),
+		global_position + Vector2(96, 32)
+	]
+
 var population
 var base_pop_growth = 0.05
 var predicted_growth
